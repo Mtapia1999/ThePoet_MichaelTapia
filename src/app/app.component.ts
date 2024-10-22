fa503316dfda58ddda3fca469e161eadd455f23e
Michael Tapia <mtapia3@uccs.edu>
2024-10-22 12:12:03 -0600

    Initial setup of styles and header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ThePoet_MichaelTapia';
+  title = 'The Poet';
+  user = 'Michael Tapia'
 }
