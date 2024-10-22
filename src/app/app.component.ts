0f986cdc04a26d4c9af13155f1721e66f7045930
Michael Tapia <mtapia3@uccs.edu>
2024-10-22 14:42:19 -0600

    Titles and Authors display depending on button clicked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,75 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Injectable } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 
+@Injectable({
+  providedIn: 'root',
+})
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'The Poet';
-  user = 'Michael Tapia'
+  user = 'Michael Tapia';
+  authorDisplay = false;
+  authorList = '';
+  titleDisplay = false;
+  titleList = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('https://poetrydb.org/author').subscribe({
+      next: (data: any) => {
+        this.authorList = data.authors;
+      },
+      error: (err) => console.log(err),
+    });
+
+    this.http.get('https://poetrydb.org/title').subscribe({
+      next: (data: any) => {
+        this.titleList = data.titles;
+      },
+      error: (err) => console.log(err),
+    });
+  }
+
+  authorButtonName(): string {
+    if (this.authorDisplay) {
+      return 'Clear';
+    } else {
+      return 'Authors';
+    }
+  }
+
+  toggleAuthor() {
+    if (this.authorDisplay) {
+      this.authorDisplay = false;
+    } else {
+      this.authorDisplay = true;
+      this.titleDisplay = false;
+    }
+  }
+
+  titleButtonName() {
+    if (this.titleDisplay) {
+      return 'Clear';
+    } else {
+      return 'Titles';
+    }
+  }
+
+  toggleTitle() {
+    if (this.titleDisplay) {
+      this.titleDisplay = false;
+    } else {
+      this.titleDisplay = true;
+      this.authorDisplay = false;
+    }
+  }
 }
